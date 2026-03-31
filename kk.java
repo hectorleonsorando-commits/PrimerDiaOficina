312a4cb4dc5b95f1932df6c1a4dd8d9cc00ab248
Leo <hectorleonsorando@gmail.com>
2026-03-31 17:36:50 +0200

    Primer programaJava

---
+++ kk.java
@@ -0,0 +1,7 @@
+public class kk {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+    }
+}
